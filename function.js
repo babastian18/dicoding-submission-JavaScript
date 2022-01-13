b50d6a1316e9eadfe61266dd324f418e058a0822
Sebastian Pandu W <89185866+babastian18@users.noreply.github.com>
2022-01-13 14:55:12 +0700

    Create function.js

---
+++ function.js
@@ -0,0 +1,25 @@
+function minimal (a,b) {
+   if (a < b) {
+   
+ return a
+ 
+ } else if (a > b) {
+ 
+ return b 
+ 
+ } else if (a === b) {
+ 
+ return a
+ 
+ }
+}
+console.log(minimal(1,9))
+console.log(minimal(8,9))
+console.log(minimal(18,18))
+
+function power (a,b) {
+  return a**b;
+}
+console.log(power(7,7))
+console.log(power(18,1))
+console.log(power(1,18))
